12ad1bf8b8d02bde21d7b28f14e82c0560c012de
jeanmartin <jeanmartinh@hotmail.fr>
2024-03-11 21:09:27 +0100

    sprint4-ticket5-feat: Système de likes en place.

--- src/Controller/Visitor/Site/SiteController.php
+++ src/Controller/Visitor/Site/SiteController.php
@@ -3,12 +3,14 @@
 namespace App\Controller\Visitor\Site;
 
 use App\Entity\Tag;
+use App\Entity\Like;
 use App\Entity\Post;
 use DateTimeImmutable;
 use App\Entity\Comment;
 use App\Entity\Category;
 use App\Form\CommentFormType;
 use App\Repository\TagRepository;
+use App\Repository\LikeRepository;
 use App\Repository\PostRepository;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -37,7 +39,7 @@ class SiteController extends AbstractController
         $posts = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1), /*page number*/
-            2 /*limit per page*/
+            10 /*limit per page*/
         );
 
         return $this->render('pages/visitor/site/index.html.twig', [
@@ -136,4 +138,60 @@ class SiteController extends AbstractController
         ]);
     }
 
+    #[Route('/site/post/{id}/{slug}/like', name: 'visitor_site_post_like', methods:['GET'])]
+    public function like(
+        Post $post,
+        EntityManagerInterface $em,
+        LikeRepository $likeRepository
+    ): Response
+    {
+        // Je return du json.
+        //Je récupère les données de l'utilisateur connecté
+        $user = $this->getUser();
+
+        // Si aucun utilisateur n'est connecté
+        if (null === $user)
+        {
+            //Je retourner ce message et code http
+            return $this->json(["message" => "Vous devez être connecté avant d'aimer un article"], 403);
+        }
+
+        // Dans le cas contraire,
+
+        // Vérifions si l'article est déjà liké
+        if ( $post->isLikedBy($user) )
+        {
+            // Récupérons le like
+            $like = $likeRepository->findOneBy(['post' => $post, 'user' => $user]);
+
+            // Supprimons-le de la table des likes
+            $em->remove($like);
+            $em->flush();
+
+            // Retournons ce message ainsi que le nombre total à jour de likes de cet article.
+            return $this->json([
+                "message" => "Le like à été retiré",
+                "totalLikesUpdated" => $likeRepository->count(['post' => $post])
+            ]);
+        }
+
+        // Dans le cas contraire,
+
+        // Créons le nouveau like
+        $like = new Like();
+        $like->setUser($user);
+        $like->setPost($post);
+
+            $like->setCreatedAt(new DateTimeImmutable());
+            $like->setUpdatedAt(new DateTimeImmutable());
+
+        $em->persist($like);
+        $em->flush();
+
+        return $this->json([
+            "message" => "Le like a été ajouté.",
+            "totalLikesUpdated" => $likeRepository->count(['post' => $post])
+        ]);
+
+    }
 }
